a2f91eb1feef5729ad3da80fd5dceab24747b8ec
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2024-09-23 11:59:52 +0300

    commit

--- frontend/collaborators/src/config/global.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface Window {
-        _app: {backendID: string; appPath: string; baseServerPath: string}
-    }
-}
-
-export const backendID = window._app?.backendID ?? '7066990350169366716'
-export const BACKEND_URL = `/custom_web_template.html?object_id=${backendID}`
